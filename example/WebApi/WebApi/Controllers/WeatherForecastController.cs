04bc5faa380c99409649340bcdbd1d1f88bec1ff
vyacheslav.sapozhnikov <vyacheslav.sapozhnikov>
2026-03-01 15:16:42 +0300

    example with retryCount = 3

---
+++ example/WebApi/WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunInBackground.SCU;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController(IBackgroundTaskManager taskManager, ILogger<WeatherForecastController> logger) : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            taskManager.Run(async (scope) =>
+            {
+                await scope.GetRequiredService<MyService>().Do();
+            }, retryCount: 3);
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ example/WebApi/WebApi/MyService.cs
@@ -0,0 +1,15 @@
+﻿namespace WebApi
+{
+    public class MyService(ILogger<MyService> logger)
+    {
+        private static int Counter = 0;
+        public async Task Do()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(10));
+            Counter++;
+            if (Counter <= 2) throw new Exception();
+            logger.LogWarning("MyService - Finished");
+            Counter = 0;
+        }
+    }
+}
